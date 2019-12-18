6561926533eb6cc8373b9639dd5058f58ca5db47
Silver Savi <silver.savi@gmail.com>
2019-12-18 13:59:43 +0200

    Initial Project add

---
+++ src/test/java/ee/slvr/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.slvr.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
