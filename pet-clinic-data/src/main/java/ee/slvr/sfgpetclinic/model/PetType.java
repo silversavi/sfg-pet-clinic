4cc67a57db6d36d7643bc1209225784864efea14
Silver Savi <silver.savi@gmail.com>
2019-12-24 00:12:35 +0200

    added PetTypeService. closes #29

--- pet-clinic-data/src/main/java/ee/slvr/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/ee/slvr/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package ee.slvr.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
---
+++ pet-clinic-data/src/main/java/ee/slvr/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package ee.slvr.sfgpetclinic.services;
+
+import ee.slvr.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
