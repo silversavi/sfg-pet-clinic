d00b5327d2ae41892cd3821e33debfaa3e2b6e1b
Silver Savi <silver.savi@gmail.com>
2019-12-24 13:55:29 +0200

    added Specialties Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/ee/slvr/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,8 @@
+package ee.slvr.sfgpetclinic.services;
+
+import ee.slvr.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+
+}
---
+++ pet-clinic-data/src/main/java/ee/slvr/sfgpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,36 @@
+package ee.slvr.sfgpetclinic.services.map;
+
+
+import ee.slvr.sfgpetclinic.model.Specialty;
+import ee.slvr.sfgpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService  extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
