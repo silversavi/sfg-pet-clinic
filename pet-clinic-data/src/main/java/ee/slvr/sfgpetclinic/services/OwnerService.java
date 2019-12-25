60833b2f48121de527710570538bc38c00379765
Silver Savi <silver.savi@gmail.com>
2019-12-25 15:23:39 +0200

    added spring data jpa service for owners. closes #40.

--- pet-clinic-data/src/main/java/ee/slvr/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/ee/slvr/sfgpetclinic/services/OwnerService.java
@@ -5,6 +5,6 @@ import ee.slvr.sfgpetclinic.model.Person;
 
 public interface OwnerService<O extends Person, L extends Number> extends CrudService<Owner, Long> {
 
-    Owner findByLastName(String lastname);
+    Owner findByLastName(String lastName);
 
 }
