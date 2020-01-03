16e5bb18a627f64316114013b33f60c414ae295d
Silver Savi <silver.savi@gmail.com>
2020-01-03 23:35:46 +0200

    added add pet, update pet #56

--- pet-clinic-data/src/main/java/ee/slvr/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/ee/slvr/sfgpetclinic/model/Owner.java
@@ -24,7 +24,9 @@ public class Owner extends Person {
         this.address = address;
         this.city = city;
         this.telephone = telephone;
-        this.pets = pets;
+        if(pets != null) {
+            this.pets = pets;
+        }
     }
 
     @Column(name = "address")
--- pet-clinic-data/src/main/java/ee/slvr/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/ee/slvr/sfgpetclinic/model/PetType.java
@@ -20,9 +20,6 @@ public class PetType extends BaseEntity {
         this.name = name;
     }
 
-
-
-
     @Column(name = "name")
     private String name;
 
